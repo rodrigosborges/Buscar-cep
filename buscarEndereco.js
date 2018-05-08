0a1b1f98b80d72efa17fb92c7fa3eb3dab1ed940
rodrigosborges <gmrodrigoborges@gmail.com>
2018-05-08 11:52:12 -0300

    ajustes

--- buscarEndereco.js
+++ buscarEndereco.js
@@ -1,38 +1,18 @@
 function buscarEndereco(){
     var cep = $('#cep').val();
 
-    $.ajax('http://api.postmon.com.br/v1/cep/' + cep)
-    .done((resposta) => {
-        console.log(resposta.bairro.toUpperCase())
+    $.ajax('http://api.postmon.com.br/v1/cep/' + cep).done((resposta) => {
         $("input[name='endereco[rua]']").val(resposta.logradouro);
         $("input[name='endereco[bairro]']").val(resposta.bairro);
-        $("select[name='endereco[municipio]']").val(resposta.cidade);
-        $("select[name='endereco[uf]']").val(resposta.estado);
+        $("input[name='endereco[municipio]']").val(resposta.cidade);
+        $("input[name='endereco[uf]']").val(resposta.estado);
         $("input[name='endereco[numero]'").val(null)
     })
-    .fail((xhr, erro) => {
-    })
-}
-function buscarEndereco2(){
-    var cep = $('#cep2').val();
-
-    $.ajax('http://api.postmon.com.br/v1/cep/' + cep)
-    .done((resposta) => {
-        $("input[name='endereco[rua]']").val(resposta.logradouro);
-        $("input[name='endereco[bairro]']").val(resposta.bairro);
-        $("select[name='endereco[municipio]']").val(resposta.cidade);
-        $("select[name='endereco[uf]']").val(resposta.estado);
-        $("input[name='endereco[numero]'").val(null)
-    })
-    .fail((xhr, erro) => {
-    })
+    .fail((xhr, erro) => {})
 }
 
 $('#cep').focusout(function(){
     buscarEndereco();
 });
 
-$('#cep2').focusout(function(){
-    buscarEndereco2();
-});
 
